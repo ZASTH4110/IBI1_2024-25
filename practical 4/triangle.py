f5645734af35a1717f6fbdb28c470bca4d268ad3
ziteng <ziteng.24@intl.zju.edu.cn>
2025-03-12 18:21:43 +0800

    .\triangle.py

--- practical 4/triangle.py
+++ practical 4/triangle.py
@@ -1,13 +1,7 @@
-# triangle.py
-# Program to calculate and display the first ten triangular numbers
-
-# Pseudocode:
 # 1. Initialize a variable x to store the sum (triangular number).
 # 2. Use a loop from 1 to 10 to calculate triangular numbers.
 # 3. In each cycle, add the current number to x.
 # 4. Print the current value of x after each addition.
-
-
 x = 0
 
 print("The first ten triangular numbers are:")
