a2e2f979d242b8d88e1d3eba4dcb1749617583ba
ziteng <ziteng.24@intl.zju.edu.cn>
2025-03-25 15:21:41 +0800

    .\population.py

--- Practical 5/population.py
+++ Practical 5/population.py
@@ -6,16 +6,16 @@ china_names = ["Zhejiang", "Fujian", "Jiangxi", "Anhui", "Jiangsu"]
 
 import matplotlib.pyplot as plt
 
-colors = ['#4db6ac', '#ffb74d', '#9575cd', '#f06292']  # 柔和配色
+colors = ['#4db6ac', '#ffb74d', '#9575cd', '#f06292']  
 plt.figure(figsize=(8, 6))
 plt.pie(uk_countries, labels=uk_names, autopct='%1.1f%%', startangle=140 ,colors=colors ,)     
 plt.title("Population Distribution in UK Countries (2022)")
-plt.axis('equal')  # 保证饼图是圆形
+plt.axis('equal')  
 plt.show()
 
-colors = ['#4db6ac', '#ffb74d', '#9575cd', '#f06292', '#64b5f6']  # 柔和配色
+colors = ['#4db6ac', '#ffb74d', '#9575cd', '#f06292', '#64b5f6'] 
 plt.figure(figsize=(8, 6))
 plt.pie(china_provinces, labels=china_names, autopct='%1.1f%%', startangle=140 ,colors=colors ,)
 plt.title("Population Distribution in China Provinces (2022)")
-plt.axis('equal')  # 保证饼图是圆形
+plt.axis('equal')
 plt.show()
