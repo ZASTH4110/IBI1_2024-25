34f5bc5a321f1ec6d34c7376bbcda335e4c71b1a
ziteng <ziteng.24@intl.zju.edu.cn>
2025-03-30 22:40:34 +0800

    .\triangle.py

--- practical 4/triangle.py
+++ practical 4/triangle.py
@@ -1,8 +1,9 @@
 # 1. Initialize a variable x to store the result.(ensure x = 0)
-# 2. Write a formula that describes the relationship between the previous triangle and the next triangle.（Tn = Tn-1 + n）
+# 2. Describes the relationship between the previous triangle and the next triangle.（Tn = Tn-1 + n）
 # 3. In each cycle, add the current number to x.（x += i）
-# 4. Print the current value of x after each addition.（x = Tn)(n=i)
+# 4. Print the current value of x after each addition.（x = Tn) (n=i)
 x = 0
+# Initialize x to 0 to store the triangular number
 
 print("The first ten triangular numbers are:")
 
@@ -10,4 +11,3 @@ print("The first ten triangular numbers are:")
 for i in range(1, 11):
     x += i  # Add current number to x
     print("n=" ,i,"  Tn =",x)  # Display the current triangular number
-    
