1895b967a4cbfb51b7331bf2a634850be962ccff
ziteng <ziteng.24@intl.zju.edu.cn>
2025-03-30 22:52:35 +0800

    .\bmi_calculator.py

--- practical 4/bmi_calculator.py
+++ practical 4/bmi_calculator.py
@@ -1,5 +1,5 @@
-# 1. Prompt the user to input their weight in kg.
-# 2. Prompt the user to input their height in meters.
+# 1. Let the user to input their weight in kg.
+# 2. Let the user to input their height in meters.
 # 3. Convert the inputs into floating-point numbers for calculation.
 # 4. Calculate BMI using the formula: BMI = weight / (height ** 2).
 # 5. Use conditional statements to determine the BMI category:
@@ -13,17 +13,17 @@ x = input("Enter your weight in kg: ")  # Get weight input
 y = input("Enter your height in m: ")  # Get height input
 
 
-w = float(x)
-h = float(y)
+w = float(x) # Convert weight to float
+h = float(y) # Convert height to float
+bmi = w / (h ** 2) # Calculate BMI
 
-bmi = w / (h ** 2)
-
+# Determine BMI category
 if bmi < 18.5:
     category = "underweight"
 elif 18.5 <= bmi < 30:
     category = "normal weight"
 else:
-    category = "obese"
+    category = "obese" 
 
-# Step 6: Print the result
-print("Your BMI is " + str(round(bmi, 2)) + ", which means you are " + category + ".")
+# Display the result
+print("Your BMI is " + str(round(bmi, 2)) + ",90 which means you are " + category + ".")
