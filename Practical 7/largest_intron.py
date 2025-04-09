6b471d9fdb7e1f593579019e6ffa6ab1c8be0271
ziteng <ziteng.24@intl.zju.edu.cn>
2025-04-09 15:10:35 +0800

    .\largest_intron.py

--- Practical 7/largest_intron.py
+++ Practical 7/largest_intron.py
@@ -1,7 +1,11 @@
+# Using re.fidnall() to find the longest possible intron in a sequence
+# the longest possible intron in a sequence
+# print
 import re as re
 seq = 'ATGCAAGTGGTGTGTCTGTTCTGAGAGGGCCTAA'
-Intron = re.findall(r'GT\S+AG', seq)
-longest_intron = max(Intron, key=len)
+Intron = re.findall(r'GT\S+AG', seq) 
+# to find GT...AG and include the GT and AG in the result.
+longest_intron = max(Intron, key=len) #find the longest intron
 print("Longest possible intron found:")
-print(f"Sequence: {longest_intron}")
-print(f"Length: {len(longest_intron)} bp")
+print(f"Sequence: {longest_intron}")# print the longest intron sequence
+print(f"Length: {len(longest_intron)} bp") # print the length of the longest intron
